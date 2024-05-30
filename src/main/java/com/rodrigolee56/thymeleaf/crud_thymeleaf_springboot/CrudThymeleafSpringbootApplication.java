5b9439ead6467d24fbd2329e578109cee581228f
RodrigoLee56 <sousarodrigo56@outlook.com>
2024-05-30 00:32:21 -0400

    Created project crud-thymeleaf-springboot

---
+++ src/main/java/com/rodrigolee56/thymeleaf/crud_thymeleaf_springboot/CrudThymeleafSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigolee56.thymeleaf.crud_thymeleaf_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudThymeleafSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudThymeleafSpringbootApplication.class, args);
+	}
+
+}
